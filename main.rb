4a37549e03dd88761e567f3e974718707d24c43e
Ben Wells <mrbrwells@gmail.com>
2014-03-10 13:19:13 -0400

    store account creds in session and redirect to list page. also viewing videos

--- main.rb
+++ main.rb
@@ -1,27 +1,41 @@
 require 'sinatra'
 require 'vimeo'
 require 'shotgun'
+enable :sessions
 
 get '/:key/:secret/:access_token/:access_token_secret/:user_id' do
 
-  consumer_key = params[:key];
-  consumer_secret = params[:secret];
-  access_token = params[:access_token];
-  access_token_secret = params[:access_token_secret];
-  user_id = params[:user_id];
+  session['ck'] = params[:key];
+  session['cs'] = params[:secret];
+  session['at'] = params[:access_token];
+  session['ats'] = params[:access_token_secret];
+  session['uid'] = params[:user_id];
   # base = Vimeo::Advanced::Base.new(consumer_key, consumer_secret, :token => access_token, :secret => access_token_secret)
   # puts base.user_id
 
+  redirect '/list/1';
+end
+
+get '/list/:page' do
   # getting and listing videos
-  video = Vimeo::Advanced::Video.new(consumer_key,
-    consumer_secret,
-    :token => access_token,
-    :secret => access_token_secret);
-    
-  @videos = video.get_all(user_id, { :page => "1",
+  video = Vimeo::Advanced::Video.new(session['ck'],
+    session['cs'],
+    :token => session['at'],
+    :secret => session['ats']);
+
+  @videos = video.get_all(session['uid'], {
+    :page => params[:page],
     :per_page => "5",
     :full_response => "1",
     :sort => "newest"
-  })
+  });
+
+  @videos = @videos['videos']['video']
   erb :index
+end
+
+get '/view/:id' do
+  @videoId = params[:id];
+  @test = session['at'];
+  erb :view;
 end
